38736ea8d6715da1520ffc0dd0c129f57f2f24a3
NathanT-W <nathtw1@gmail.com>
2019-03-04 12:01:59 +0000

    Clarify constraints

--- scripts/constraint_1.sql
+++ scripts/constraint_1.sql
@@ -3,18 +3,24 @@
 * CSY1026 Databases 1 - Assignment 2
 */
 
---CREATE CONSTRAINTS
-PROMPT Altering 'employees' table
+--CREATE Primary Keys
+PROMPT Primary Key 'employees' table
 ALTER TABLE employees
 ADD CONSTRAINT pk_employees
 PRIMARY KEY (employee_id);
 
-PROMPT Altering 'specialisms' table
+PROMPT Primary Key 'specialisms' table
 ALTER TABLE specialisms
 ADD CONSTRAINT pk_specialisms
 PRIMARY KEY (specialism_id);
 
-PROMPT Altering 'employee_specialisms' table
+PROMPT Primary key 'employee_specialisms' table
+ALTER TABLE employee_specialisms
+ADD CONSTRAINT pk_employee_specialisms
+PRIMARY KEY (employee_id, specialism_id);
+
+--CREATE Foreign Keys
+PROMPT Foreign Keys 'employee_specialisms' table
 ALTER TABLE employee_specialisms
 ADD CONSTRAINT fk_e_employee_specialisms
 FOREIGN KEY (employee_id)
@@ -24,7 +30,3 @@ ALTER TABLE employee_specialisms
 ADD CONSTRAINT fk_s_employee_specialisms
 FOREIGN KEY (specialism_id)
 REFERENCES specialisms(specialism_id);
-
-ALTER TABLE employee_specialisms
-ADD CONSTRAINT pk_employee_specialisms
-PRIMARY KEY (employee_id, specialism_id);
