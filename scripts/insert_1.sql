2aeebe43ca46668a0deca6e216842f3224f828e5
Alexander Turner <cloud-alex@hotmail.co.uk>
2019-03-04 11:37:43 +0000

    Added an insert for the 'specialisms' table.

--- scripts/insert_1.sql
+++ scripts/insert_1.sql
@@ -5,18 +5,11 @@
 
 --INSERT INTO TABLES
 --Insert into 'employees' table
-INSERT INTO employees VALUES(
-seq_employees.NEXTVAL,
-'MR',
-'JOHN',
-'DOE',
-'M',
-'john@doe.com',
-'01234567899',
-'99876543210',
-'101',
-'RANDOM STREET',
-'NORTHAMPTON',
-'NORTHAMPTONSHIRE',
-'NN15PH',
-'GB');
+PROMPT Inserting data into the 'employees' table
+INSERT INTO employees 
+VALUES(seq_employees.NEXTVAL, 'MR', 'JOHN', 'DOE', 'M', 'john@doe.com', '01234567899', '99876543210', '101', 'RANDOM STREET', 'NORTHAMPTON', 'NORTHAMPTONSHIRE', 'NN15PH', 'GB');
+
+--Insert into 'specialisms' table
+PROMPT Inserting data into the 'specialisms' table
+INSERT INTO specialisms 
+VALUES(seq_specialisms.NEXTVAL, 'TEST', 'This is a test.');
