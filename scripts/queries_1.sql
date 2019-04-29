eda8b924422cf19088e4657234173ec8c9ca45df
Nathan <45460974+YuiKirigaya@users.noreply.github.com>
2019-04-29 11:49:56 +0100

    Update queries_1.sql

--- scripts/queries_1.sql
+++ scripts/queries_1.sql
@@ -15,7 +15,7 @@ SELECT gender FROM employees
 WHERE gender = 'M';
 
 SELECT city FROM employees
-WHERE city = ('TOWCESTER', 'HARLOW', 'NEATH', 'SOFIA', 'DAYTON', 'NEW YORK CITY');
+WHERE city = ('DAYTON', 'HARLOW', 'NEW YORK CITY', 'NEATH', 'SOFIA', 'TOWCESTER');
 
 SELECT meeting_date FROM meetings
 WHERE meeting_date = ('JAN', 'FEB', 'MAR');
