8b8a5fe29133dfbc626da6de6a787c6a6265c97d
NathanT-W <nathtw1@gmail.com>
2019-04-29 12:29:01 +0100

    Final query

--- scripts/queries_1.sql
+++ scripts/queries_1.sql
@@ -17,10 +17,20 @@ WHERE gender = 'M';
 SELECT title,firstname,surname, city FROM employees
 WHERE city <> 'NORTHAMPTON';
 
-SELECT  meeting_id, meeting_date FROM meetings
+SELECT meeting_id, meeting_date FROM meetings
 WHERE meeting_date BETWEEN '01-JAN-2019' AND '01-APR-2019';
 
-SELECT employees.firstname, employees.surname,
+SELECT employees.firstname, employees.surname, project_teams.project_id
+FROM employees JOIN project_teams
+ON employees.employee_id = project_teams.employee_id
+WHERE project_teams.team_id = 00000001;
+
+SELECT employees.firstname, employees.surname, project_teams.team_id, projects.project_id
+FROM employees JOIN project_teams
+ON employees.employee_id = project_teams.employee_id
+JOIN projects
+ON projects.project_id = project_teams.project_id
+WHERE project_teams.team_id = 00000001 OR employees.gender = 'F';
 
 SELECT title, firstname, surname FROM employees ORDER BY surname DESC;
 
